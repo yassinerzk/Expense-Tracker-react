91380202f6b13c0bcc87dcf6417dfda249dc9dae
yassinerzk <yassinerazzouki@gmail.com>
2020-02-25 03:28:51 +0100

    Updates

---
+++ precache-manifest.80b8e87d9c61b47fb96a75d86345dd34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b656ed74cd0674f83950a2635aa337ee",
+    "url": "/Expense-Tracker-react-/index.html"
+  },
+  {
+    "revision": "1c8ddc39e8084812446b",
+    "url": "/Expense-Tracker-react-/static/css/main.3aab3814.chunk.css"
+  },
+  {
+    "revision": "6c4a503a07e7c39835e6",
+    "url": "/Expense-Tracker-react-/static/js/2.fe0fce3d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Expense-Tracker-react-/static/js/2.fe0fce3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c8ddc39e8084812446b",
+    "url": "/Expense-Tracker-react-/static/js/main.8fbd09f9.chunk.js"
+  },
+  {
+    "revision": "ab95d0b55fb03a0edce5",
+    "url": "/Expense-Tracker-react-/static/js/runtime-main.67923744.js"
+  }
+]);
